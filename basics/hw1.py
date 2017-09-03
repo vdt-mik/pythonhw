6e3ac95ffd8a534ed27e7178b366998bec740fbf
vdt-mik <vdt-mik@alterpost.org>
2017-09-03 21:32:20 +0300

    add home work file

---
+++ basics/hw1.py
@@ -0,0 +1,20 @@
+# task_one
+def task_one(arg1):
+	print '"Hello, {} !"'.format(arg1)
+
+task_one("Hello")
+
+# task_two
+def sum(arg1,arg2,arg3,arg4):
+	sum = arg1 + arg2 + arg3 + arg4
+	print '{}'.format(sum)
+sum(1,2,3,4)
+def multiply(arg1,arg2,arg3,arg4):
+	multiply = arg1 * arg2 * arg3 * arg4
+	print '{}'.format(multiply)
+multiply(1,2,3,4)
+#task three
+def reverse(arg1):
+	print arg1[::-1]
+reverse("hello")
+# task_four
